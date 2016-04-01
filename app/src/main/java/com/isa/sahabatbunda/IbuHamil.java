c10750dbb1bb61017ec6262cef288b9ac359c6b4
iwannabd <azaqokay@gmail.com>
2016-04-01 16:50:45 +0700

    Initial commit

---
+++ app/src/main/java/com/isa/sahabatbunda/IbuHamil.java
@@ -0,0 +1,13 @@
+package com.isa.sahabatbunda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IbuHamil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ibu_hamil);
+    }
+}
---
+++ app/src/main/java/com/isa/sahabatbunda/OnFragmentInteractionListener.java
@@ -0,0 +1,12 @@
+package com.isa.sahabatbunda;
+
+import android.net.Uri;
+
+/**
+ * Created by node06 on 29/03/2016.
+ */
+public interface OnFragmentInteractionListener {
+
+    void doSomething(Uri uri);
+
+}
